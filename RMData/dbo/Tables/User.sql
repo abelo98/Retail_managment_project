c8ddc67f7e6f61235a0803fc53838d9cd370be16
Abel <abelo08cruz@gmail.com>
2022-08-18 03:01:45 +0200

    Register DB tables
    Tables for the basic register system

---
+++ RMData/dbo/Tables/User.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[User]
+(
+    [Id] NCHAR(128) NOT NULL PRIMARY KEY, 
+    [Firstname] BINARY(50) NOT NULL, 
+    [LastName] NCHAR(50) NOT NULL, 
+    [EmailAddress] NCHAR(256) NOT NULL, 
+    [CreateDate] DATETIME2 NULL DEFAULT getutcdate()
+)
