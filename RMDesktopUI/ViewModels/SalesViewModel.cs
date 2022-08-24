82ba82ea9ba11d4d4b5ea56549ffa7084890224f
Abel <abelo08cruz@gmail.com>
2022-08-24 12:57:39 -0400

    Adds Sales Page
    Adds the Sales page Xaml and the supporting
    View Model. Just Scaffolds out of VM, does not add actual logic.

---
+++ RMDesktopUI/ViewModels/SalesViewModel.cs
@@ -0,0 +1,121 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMDesktopUI.ViewModels
+{
+    public class SalesViewModel:Screen
+    {
+        private BindingList<string> _products;
+
+        public BindingList<string> Products
+        {
+            get { return _products; }
+            set
+            {
+                _products = value; 
+                NotifyOfPropertyChange(() => Products);
+            }
+        }
+
+        private BindingList<string> _cart;
+
+        public BindingList<string> Cart
+        {
+            get { return _cart; }
+            set
+            {
+                _cart = value;
+                NotifyOfPropertyChange(() => Cart);
+            }
+        }
+
+        private string _itemQuantity;
+
+        public string ItemQuantity
+        {
+            get { return _itemQuantity; }
+            set
+            {
+                _itemQuantity = value;
+                NotifyOfPropertyChange(() => ItemQuantity);
+            }
+        }
+
+        private string _subtotal;
+
+        public string Subtotal
+        {
+            get
+            {
+                // TODO: replace for calculations
+                return "$0.00";
+            }
+        }
+
+        private string _tax;
+
+        public string Tax
+        {
+            get
+            {
+                // TODO: replace for calculations
+                return "$0.00";
+            }
+        }
+
+        private string _total;
+
+        public string Total
+        {
+            get
+            {
+                // TODO: replace for calculations
+                return "$0.00";
+            }
+        }
+
+
+        public bool CanAddToCart
+        {
+            get
+            {
+                bool output = false;
+                // Somthing is selected
+                // There is the qty of the product
+                return output;
+            }
+        }
+
+        public void AddToCart() { }
+
+        public bool CanRemoveFromCart
+        {
+            get
+            {
+                bool output = false;
+                // Somthing is selected
+                return output;
+            }
+        }
+
+        public void RemoveFromCart() { }
+
+        public bool CanCheckOut
+        {
+            get
+            {
+                bool output = false;
+                // Somthing is selected in the cart
+                return output;
+            }
+        }
+
+        public void CheckOut() { }
+
+    }
+}
