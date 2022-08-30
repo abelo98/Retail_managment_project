00518f743dfabea3868503e2a2e08df271781941
Abel <abelo08cruz@gmail.com>
2022-08-29 21:29:27 -0400

    replace container reference in ShellViewModel
    replaces the container reference for the IoC instead.

--- RMDesktopUI/ViewModels/ShellViewModel.cs
+++ RMDesktopUI/ViewModels/ShellViewModel.cs
@@ -5,19 +5,18 @@ using System.Threading.Tasks;
 
 namespace RMDesktopUI.ViewModels
 {
-    public class ShellViewModel: Conductor<object>,IHandle<LogOnEvent>
+    public class ShellViewModel : Conductor<object>, IHandle<LogOnEvent>
     {
         private SalesViewModel _salesViewModel;
-        private SimpleContainer _container;
+
         private IEventAggregator _events;
-        public ShellViewModel(SimpleContainer container, IEventAggregator events, SalesViewModel salesViewModel)
+        public ShellViewModel(IEventAggregator events, SalesViewModel salesViewModel)
         {
             _events = events;
-            _container = container;
             _salesViewModel = salesViewModel;
 
             _events.SubscribeOnPublishedThread(this);
-            ActivateItemAsync(_container.GetInstance<LoginViewModel>());
+            ActivateItemAsync(IoC.Get<LoginViewModel>());
         }
 
         public async Task HandleAsync(LogOnEvent message, CancellationToken cancellationToken)
